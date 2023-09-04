9ab1f939385205f39c9f4b885ad9b59c3c08794e
MichalskiLucas <lucasmichalski.pessoal@gmail.com>
2023-09-04 20:19:06 -0300

    Commit do dia 04/09/2023

--- ForcaVendas/app/src/main/java/br/com/unipar/forcavendas/EnderecoActivity.java
+++ ForcaVendas/app/src/main/java/br/com/unipar/forcavendas/EnderecoActivity.java
@@ -4,10 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ItemActivity extends AppCompatActivity {
+public class EnderecoActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_item);
+        setContentView(R.layout.activity_endereco);
     }
 }
